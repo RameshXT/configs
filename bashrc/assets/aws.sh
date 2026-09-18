640719d2032b3b3b245f1bd215a0975e20087da0
Ramesh <ramesh.kanna@smaitic.com>
2026-09-18 21:57:29 +0530

    fix(naming): standardize display name to Smaitic Venture

--- bashrc/assets/aws.sh
+++ bashrc/assets/aws.sh
@@ -10,7 +10,7 @@ _aws_pick_session() {
       else
         case "$name" in
           smaitic) label="Smaitic Labs" ;;
-          smaitik) label="Smaitik Venture" ;;
+          smaitik) label="Smaitic Venture" ;;
           *) label="$name" ;;
         esac
       fi
@@ -103,7 +103,7 @@ _aws_get_display_name() {
 
   case "$session" in
     smaitic) echo "Smaitic Labs" ;;
-    smaitik) echo "Smaitik Venture" ;;
+    smaitik) echo "Smaitic Venture" ;;
     *) echo "$session" ;;
   esac
 }
